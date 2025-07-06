67a3a877f14dfb4a5e513583e01c32f72a98e220
Dinosaur <1950518@tongji.edu.cn>
2025-07-06 13:01:52 +0800

    Compare list

--- Helloworld.py
+++ Helloworld.py
@@ -14,23 +14,28 @@ print(np.random.randint(1,9))
 
 # Generate a vector [1,2,3,4,5]
 v = np.array([1,2,3,4,5])
-u = np.array(range(5))
-w = np.linspace(1,5,5)
+u = np.linspace(1,5,5)
+w = range(5)
+x = list(range(1,5,2))
 print(v)
-print(u)
-print(w)
-
 print(v[0])
 print(v[1:3])
 print(v[:2])
 print(v[:5])
 print(v[1:])
 
+print(u)
+print(w)
+print(x)
+print(x[1])
+
+
 # Generate a matrix [[1,2,3],[4,5,6],[7,8,9]]
 m = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print(m)
 # Output m(1,2) = 2
 print(m[0,1])
+print(m[1][2])
 
 # Draw a sine wave
 x = np.linspace(0, 2*np.pi, 100)
@@ -45,5 +50,3 @@ plt.legend()
 end_time = time.time()
 total_time = end_time - start_time
 print(f"\nTotal execution time: {total_time:.4f} seconds")
-
-plt.show()
